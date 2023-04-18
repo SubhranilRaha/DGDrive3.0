bd8514d1a215227fa2abc368cbdc4fbdbf121eac
SubhranilRaha <82709110+SubhranilRaha@users.noreply.github.com>
2023-04-18 23:09:36 +0530

    sharing added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const ALCHEMY_API_KEY=process.env.GOERLI_URL;
+const SEPOLIA_PRIVATE_KEY=process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.9",
+//   networks: {
+//     hardhat: {
+//       chainId: 1337,
+//     },
+//     goerli:{
+//       url: GOERLI_URL,
+//       accounts: [PRIVATE_KEY],
+//     }
+//   },
+//   paths: {
+//     artifacts: "./client/src/artifacts",
+//   },
+// };
+
+
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    hardhat: {
-      chainId: 1337,
-    },
-  },
-  paths: {
-    artifacts: "./client/src/artifacts",
-  },
+    sepolia: {
+      url: ALCHEMY_API_KEY,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
